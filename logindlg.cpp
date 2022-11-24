ee468ad89e06c74725747754682dcc1e772a74b4
lgh <2524398864@qq.com>
2022-11-24 22:19:29 +0800

    feat: 添加登录主窗口

---
+++ logindlg.cpp
@@ -0,0 +1,16 @@
+#include "logindlg.h"
+#include "ui_logindlg.h"
+
+LoginDlg::LoginDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginDlg)
+{
+    ui->setupUi(this);
+}
+
+LoginDlg::~LoginDlg()
+{
+    delete ui;
+}
+
+
---
+++ logindlg.h
@@ -0,0 +1,22 @@
+#ifndef LOGINDLG_H
+#define LOGINDLG_H
+
+#include <QDialog>
+
+namespace Ui {
+class LoginDlg;
+}
+
+class LoginDlg : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LoginDlg(QWidget *parent = nullptr);
+    ~LoginDlg();
+
+private:
+    Ui::LoginDlg *ui;
+};
+
+#endif // LOGINDLG_H
